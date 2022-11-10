b3a40f2a4a5bb9e0b4bd67ef65f649ff3baa92b9
Vinicius José <viniciusjosedev@gmail.com>
2022-11-10 17:21:46 -0300

    mais atualizações

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import './style.css' 
-const urlapi = 'https://api.exchangerate.host/latest${base=BRL}';
 
-fetch(urlapi).then(resultado => resultado.json()).then(resultado => console.log(resultado))
+const EUR = 'https://api.exchangerate.host/latest${base=EUR}';
+const USD = 'https://api.exchangerate.host/latest?base=USD';
+const BRL = 'https://api.exchangerate.host/latest?base=BRL';
+
+const button = document.querySelector('button');
+const input = document.querySelector('#input-text');
+const divs = document.querySelector('valores');
+
+
+button.addEventListener('click', (event) => {
+	event.preventDefault();
+	if (input.value === 'EUR') {
+    
+        fetch(EUR).then(resultado => resultado.json()).then(resultado => console.log(resultado))
+	}
+})
+
+// fetch(EUR).then(resultado => resultado.json()).then(resultado => console.log(resultado))
+
+
 
