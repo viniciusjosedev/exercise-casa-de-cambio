65b68f094179df6a6872da84d433a3d606416aa2
Vinicius José <viniciusjosedev@gmail.com>
2022-11-10 17:09:00 -0300

    mais atualizacações

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './style.css' 
-const urlapi = 'https://api.exchangerate.host/latest';
+const urlapi = 'https://api.exchangerate.host/latest${base=BRL}';
 
-
-
+fetch(urlapi).then(resultado => resultado.json()).then(resultado => console.log(resultado))
 
