ee8bd4866806cc73e0302798d4325a3b1b858021
Vinicius José <viniciusjosedev@gmail.com>
2022-11-10 22:26:55 -0300

    ajustes sendo feitos

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-import './style.css' 
+import './style.css';
 
-const EUR = 'https://api.exchangerate.host/latest${base=EUR}';
-const USD = 'https://api.exchangerate.host/latest?base=USD';
-const BRL = 'https://api.exchangerate.host/latest?base=BRL';
+const button = document.getElementsByTagName('button')[0]
+const api = 'https://api.exchangerate.host/latest?'
 
-const button = document.querySelector('button');
-const input = document.querySelector('#input-text');
-const divs = document.querySelector('valores');
+function requireAPI() {
+	const input = document.getElementsByTagName('input')[0]
+  return fetch(api + `base=${input.value}`).then(resultado => resultado.json())
+	.then(resultado => resultado.rates)
+}
 
+function addItens() {
+	const resultado = requireAPI().then(resultado => resultado)
+	console.log(resultado)
+}
 
-button.addEventListener('click', (event) => {
-	event.preventDefault();
-	if (input.value === 'EUR') {
-    
-        fetch(EUR).then(resultado => resultado.json()).then(resultado => console.log(resultado))
-	}
-})
-
-// fetch(EUR).then(resultado => resultado.json()).then(resultado => console.log(resultado))
-
+button.addEventListener('click', addItens)
 
 
